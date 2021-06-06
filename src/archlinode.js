e310c846a2ab5d1c67683036e1b45c2efbe37ffd
Aaron's Dev <super.conctacto@gmail.com>
2021-06-06 10:59:18 +0200

    Removing use of bash scripts on normal instalation

--- src/archlinode.js
+++ src/archlinode.js
@@ -1,7 +1,6 @@
 const prompt = require("prompts"),
 	chalk = require("chalk"),
-	{ exec } = require("child_process"),
-	fs = require("graceful-fs");
+	{ exec, execSync } = require("child_process");
 
 (async function () {
 	const questions = [
@@ -130,20 +129,30 @@ const prompt = require("prompts"),
 		return process.exit(0);
 	}
 
-	const write = async _ => {
-		await fs.appendFileSync("install", `${_}\n`);
+	const run = async _ => {
+		await execSync(`${_}`, (error, stdout, stderr) => {
+			if (error) {
+				console.log(chalk.redBright("[ERROR / IGNORE]") + `An error ocurred, if it isn't anything strange, you should ignore it\nError: ${error}`);
+			}
+
+			try {
+				callback(stdout);
+			} catch (err) {
+				console.log(chalk.redBright("[IGNORE] ") + "Couldn't get callback answer");
+			}
+		});
 	}
 
 	const pacman = async _ => {
-		await write(`arch-chroot /mnt pacman -S ${_} --noconfirm --needed`);
+		await run(`arch-chroot /mnt pacman -S ${_} --noconfirm --needed`);
 	}
 
 	const pacstrap = async _ => {
-		await write(`pacstrap /mnt ${_}`)
+		await run(`pacstrap /mnt ${_}`)
 	}
 
 	const pip = async _ => {
-		await write(`arch-chroot /mnt pip install ${_}`);
+		await run(`arch-chroot /mnt pip install ${_}`);
 	}
 
 	const install = async (_) => {
@@ -151,62 +160,62 @@ const prompt = require("prompts"),
 
 		switch (_.keymap) {
 			case 0:
-				await write("loadkeys en");
+				await run("loadkeys en");
 				break;
 			case 1:
-				await write("loadkeys es");
+				await run("loadkeys es");
 				break;
 			default:
 				break;
 		}
 
-		await write("timedatectl set-ntp true");
-		await write(`mkfs.ext4 /dev/sda${_.sda}`);
-		await write(`mount /dev/sda${_.sda} /mnt`);
-		await write(`mkdir /mnt/efi`);
-		await write(`mount /dev/sda${_.efi} /mnt/efi`);
+		await run("timedatectl set-ntp true");
+		await run(`mkfs.ext4 /dev/sda${_.sda}`);
+		await run(`mount /dev/sda${_.sda} /mnt`);
+		await run(`mkdir /mnt/efi`);
+		await run(`mount /dev/sda${_.efi} /mnt/efi`);
 		await pacstrap(`base ${_.kernel} linux-firmware`);
-		await write(`genfstab -U /mnt >> /mnt/etc/fstab`)
-		await write(`echo ${_.hostname} >> /mnt/etc/hostname`);
-		await write(`echo 127.0.0.1 localhost >> /mnt/etc/hosts`);
-		await write(`echo ::1 localhost >> /mnt/etc/hosts`);
-		await write(`arch-chroot /mnt mkdir /home/build`);
-		await write(`arch-chroot /mnt sh -c 'chgrp nobody /home/build && chmod g+ws /home/build && setfacl -m u::rwx,g::rwx /home/build && setfacl -d --set u::rwx,g::rwx,o::- /home/build'`)
-		await write(`arch-chroot /mnt hwclock --systohc`)
-		await write(`arch-chroot /mnt locale-gen`);
-		await write(`arch-chroot /mnt useradd --create-home ${_.username}`);
-		await write(`arch-chroot /mnt usermod -aG wheel,video,audio,storage ${_.username}`);
-		await write(`rm /mnt/etc/sudoers`);
-		await write(`curl -L is.gd/archlnsd >> /mnt/etc/sudoers`);
-		await write(`arch-chroot /mnt chmod -c 0440 /etc/sudoers`);
-		await write(`arch-chroot /mnt chown -c root:root /etc/sudoers`);
-		await write(`rm -rf /mnt/etc/pacman.conf`);
-		await write(`curl -L is.gd/arlnpcf >> /mnt/etc/pacman.conf`);
-		await write(`arch-chroot /mnt chmod -c 644 /etc/pacman.conf`);
-		await write(`arch-chroot /mnt chown -c root:root /etc/pacman.conf`);
-		await write(`arch-chroot /mnt pacman -Syu`);
+		await run(`genfstab -U /mnt >> /mnt/etc/fstab`)
+		await run(`echo ${_.hostname} >> /mnt/etc/hostname`);
+		await run(`echo 127.0.0.1 localhost >> /mnt/etc/hosts`);
+		await run(`echo ::1 localhost >> /mnt/etc/hosts`);
+		await run(`arch-chroot /mnt mkdir /home/build`);
+		await run(`arch-chroot /mnt sh -c 'chgrp nobody /home/build && chmod g+ws /home/build && setfacl -m u::rwx,g::rwx /home/build && setfacl -d --set u::rwx,g::rwx,o::- /home/build'`)
+		await run(`arch-chroot /mnt hwclock --systohc`)
+		await run(`arch-chroot /mnt locale-gen`);
+		await run(`arch-chroot /mnt useradd --create-home ${_.username}`);
+		await run(`arch-chroot /mnt usermod -aG wheel,video,audio,storage ${_.username}`);
+		await run(`rm /mnt/etc/sudoers`);
+		await run(`curl -L is.gd/archlnsd >> /mnt/etc/sudoers`);
+		await run(`arch-chroot /mnt chmod -c 0440 /etc/sudoers`);
+		await run(`arch-chroot /mnt chown -c root:root /etc/sudoers`);
+		await run(`rm -rf /mnt/etc/pacman.conf`);
+		await run(`curl -L is.gd/arlnpcf >> /mnt/etc/pacman.conf`);
+		await run(`arch-chroot /mnt chmod -c 644 /etc/pacman.conf`);
+		await run(`arch-chroot /mnt chown -c root:root /etc/pacman.conf`);
+		await run(`arch-chroot /mnt pacman -Syu`);
 		await pacman(`pulseaudio dhcpcd xterm networkmanager nano thunar python python-pip python3 base-devel grub os-prober efibootmgr gcc cmake make git curl libx11 libxft xorg xorg-server xorg-xinit terminus-font mesa`)
 		
 		switch (_.keymap) {
 			case 0:
-				await write("arch-chroot /mnt localectl set-x11-keymap en");
+				await run("arch-chroot /mnt localectl set-x11-keymap en");
 				break;
 			case 1:
-				await write("arch-chroot /mnt localectl set-x11-keymap es");
+				await run("arch-chroot /mnt localectl set-x11-keymap es");
 				break;
 			default:
 				break;
 		}
 
-		await write(`arch-chroot /mnt git clone https://aur.archlinux.org/yay-bin.git /home/build/yay`);
-		await write(`arch-chroot /mnt sh -c 'cd /home/build/yay && sudo -u nobody makepkg -s && pacman -U *.tar.zst --noconfirm'`);
+		await run(`arch-chroot /mnt git clone https://aur.archlinux.org/yay-bin.git /home/build/yay`);
+		await run(`arch-chroot /mnt sh -c 'cd /home/build/yay && sudo -u nobody makepkg -s && pacman -U *.tar.zst --noconfirm'`);
 		
 		if (_.apps.length >= 1) {
 			await _.apps.forEach(async element => {
 				if (element == "pycritty-checking") {
 					await pip(`pycritty`);
 				} else if (element == "doom-checking") {
-					await write(`arch-chroot /mnt sh -c 'pacman -S emacs --noconfirm && git clone https://github.com/hlissner/doom-emacs /home/build/doom'`)
+					await run(`arch-chroot /mnt sh -c 'pacman -S emacs --noconfirm && git clone https://github.com/hlissner/doom-emacs /home/build/doom'`)
 				} else if (element == "virtualbox-checking") {
 					if (_.kernel == "linux") {
 						await pacman(`virtualbox virtualbox-host-modules-arch virtualbox-guest-iso`)
@@ -246,14 +255,14 @@ const prompt = require("prompts"),
 						break;
 					case "dwm":
 						await pacman(`dmenu`);
-						await write(`arch-chroot /mnt mkdir /home/build/config`);
-						await write(`arch-chroot /mnt git clone https://aur.archlinux.org/dwm-git.git /home/build/dwm-git`);
-						await write(`arch-chroot /mnt git clone https://github.com/antoniosarosi/dwm.git /home/build/dwm`);
-						await write(`arch-chroot /mnt git clone https://aur.archlinux.org/st.git /home/build/st`);
-						await write(`curl -L https://raw.githubusercontent.com/skimbledevs/archlinode/main/scripts/dwm.sh >> /mnt/home/build/config/dwm.sh`)
-						await write(`arch-chroot /mnt sh -c 'cd /home/build/st && sudo -u nobody makepkg -s && pacman -U *.tar.zst --noconfirm'`)
-						await write(`arch-chroot /mnt sh -c 'cd /home/build/dwm-git && sudo -u nobody makepkg -s && pacman -U *.tar.zst --noconfirm'`);
-						await write(`arch-chroot /mnt sh -c 'cd /home/build/config && sh dwm.sh'`)
+						await run(`arch-chroot /mnt mkdir /home/build/config`);
+						await run(`arch-chroot /mnt git clone https://aur.archlinux.org/dwm-git.git /home/build/dwm-git`);
+						await run(`arch-chroot /mnt git clone https://github.com/antoniosarosi/dwm.git /home/build/dwm`);
+						await run(`arch-chroot /mnt git clone https://aur.archlinux.org/st.git /home/build/st`);
+						await run(`curl -L https://raw.githubusercontent.com/skimbledevs/archlinode/main/scripts/dwm.sh >> /mnt/home/build/config/dwm.sh`)
+						await run(`arch-chroot /mnt sh -c 'cd /home/build/st && sudo -u nobody makepkg -s && pacman -U *.tar.zst --noconfirm'`)
+						await run(`arch-chroot /mnt sh -c 'cd /home/build/dwm-git && sudo -u nobody makepkg -s && pacman -U *.tar.zst --noconfirm'`);
+						await run(`arch-chroot /mnt sh -c 'cd /home/build/config && sh dwm.sh'`)
 						break;
 					case "plasma":
 						await pacman(`plasma kde-applications`)
@@ -276,42 +285,42 @@ const prompt = require("prompts"),
 			});
 		}
 
-		await write(`arch-chroot /mnt sh -c 'git clone https://aur.archlinux.org/nerd-fonts-ubuntu-mono.git /home/build/nerdfonts && cd /home/build/nerdfonts && sudo -u nobody makepkg -s && pacman -U *.tar.zst'`)
+		await run(`arch-chroot /mnt sh -c 'git clone https://aur.archlinux.org/nerd-fonts-ubuntu-mono.git /home/build/nerdfonts && cd /home/build/nerdfonts && sudo -u nobody makepkg -s && pacman -U *.tar.zst'`)
 
 		switch (_.lmanager) {
 			case 0:
 				await pacman(`lxdm`);
-				await write(`arch-chroot /mnt systemctl enable lxdm.service`);
+				await run(`arch-chroot /mnt systemctl enable lxdm.service`);
 				break;
 			case 1:
 				await pacman(`gdm`);
-				await write(`arch-chroot /mnt systemctl enable gdm.service`);
+				await run(`arch-chroot /mnt systemctl enable gdm.service`);
 				break;
 			case 2:
-				await write(`arch-chroot /mnt git clone https://aur.archlinux.org/ly.git /home/build/ly`);
-				await write(`arch-chroot /mnt sh -c 'cd /home/build/ly && sudo -u nobody makepkg -s && pacman -U *.tar.zst'`)
-				await write(`arch-chroot /mnt systemctl enable ly.service`)
+				await run(`arch-chroot /mnt git clone https://aur.archlinux.org/ly.git /home/build/ly`);
+				await run(`arch-chroot /mnt sh -c 'cd /home/build/ly && sudo -u nobody makepkg -s && pacman -U *.tar.zst'`)
+				await run(`arch-chroot /mnt systemctl enable ly.service`)
 				break;
 			case 3:
 				await pacman(`lightdm lightdm-gtk-greeter`);
-				await write(`arch-chroot /mnt systemctl enable lightdm.service`);
+				await run(`arch-chroot /mnt systemctl enable lightdm.service`);
 				break;
 			default:
 				break;
 		}
 		
-		await write(`curl -L is.gd/arlndpi >> postinstall`);
-		await write(`curl -L is.gd/arlnrb >> reboot`);
-		await write(`chmod +x reboot`);
+		await run(`curl -L is.gd/arlndpi >> postinstall`);
+		await run(`curl -L is.gd/arlnrb >> reboot`);
+		await run(`chmod +x reboot`);
 		await pacman(`rofi thunar firefox alacritty redshift scrot which feh ttf-dejavu ttf-liberation noto-fonts pulseaudio pavucontrol pamixer brightnessctl arandr udiskie ntfs-3g volumeicon cbatticon libnotify notification-daemon`)
-		await write(`arch-chroot /mnt fc-cache -f -v`)
-		await write(`arch-chroot /mnt echo "[D-BUS Service]\nName=org.freedesktop.Notifications\nExec=/usr/lib/notification-daemon-1.0/notification-daemon" >> /usr/share/dbus-1/services/org.freedesktop.Notifications.service`)
-		await write(`arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=ArchLiNode`);
-		await write(`arch-chroot /mnt os-prober`);
-		await write(`arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg`);
-		await write(`arch-chroot /mnt systemctl enable NetworkManager`);
-		await write(`clear && chmod +x postinstall && chmod +x reboot && ./postinstall`);
-		await write(`rm postinstall && rm archlinode`);
+		await run(`arch-chroot /mnt fc-cache -f -v`)
+		await run(`arch-chroot /mnt echo "[D-BUS Service]\nName=org.freedesktop.Notifications\nExec=/usr/lib/notification-daemon-1.0/notification-daemon" >> /usr/share/dbus-1/services/org.freedesktop.Notifications.service`)
+		await run(`arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=ArchLiNode`);
+		await run(`arch-chroot /mnt os-prober`);
+		await run(`arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg`);
+		await run(`arch-chroot /mnt systemctl enable NetworkManager`);
+		await run(`clear && chmod +x postinstall && chmod +x reboot && ./postinstall`);
+		await run(`rm postinstall && rm archlinode`);
 	}
 
 	let _ = await prompt([{ type: "confirm", name: "net", message: "Are you connected to the internet using a ethernet / LAN cable?"}], { onCancel });
@@ -320,9 +329,8 @@ const prompt = require("prompts"),
 		case true:
 			_ = await prompt(questions, { onCancel });
 			install(_).then(() => {
-				exec('chmod +x install', (error, stdout, stderr) => {
+				execSync('chmod +x install', (error, stdout, stderr) => {
 					if (error) throw new Error(error);
-					if (stderr) throw new Error(stderr);
 				});
 			});
 			break;
@@ -338,9 +346,8 @@ const prompt = require("prompts"),
 			setTimeout(async () => {
 				_ = await prompt(questions, { onCancel });
 				install(_).then(() => {
-					exec('chmod +x install', (error, stdout, stderr) => {
+					execSync('chmod +x install', (error, stdout, stderr) => {
 						if (error) throw new Error(error);
-						if (stderr) throw new Error(stderr);
 					});
 				});
 			}, 30000)
